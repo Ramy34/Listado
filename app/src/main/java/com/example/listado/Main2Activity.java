90be2d22d5b5460e54d15ec443c3ebfd5f7c3bd0
Ramsés Castro <ramses-c14@live.com.mx>
2020-04-23 21:13:46 -0500

    Se genera la navegación entre activity

--- app/src/main/java/com/example/listado/Main2Activity.java
+++ app/src/main/java/com/example/listado/Main2Activity.java
@@ -2,13 +2,35 @@ package com.example.listado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+    Button btnG, btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btnG = findViewById(R.id.btnG);
+        btnReg = findViewById(R.id.btnReg);
+
+        btnG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Main2Activity.this,"Se guardó el mensaje", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/listado/MainActivity.java
+++ app/src/main/java/com/example/listado/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.listado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnR, btnC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnC = findViewById(R.id.btC);
+        btnR = findViewById(R.id.btR);
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
